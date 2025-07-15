057d3cf7239dba7d4b6317be48f57afda553f0d1
Thomas Volden <thvo@itu.dk>
2025-07-15 12:46:23 +0200

    Added configurations to include razor files and data

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddContentManagement(builder.Environment.ContentRootPath, builder.Configuration, Assembly.GetAssembly(typeof(Program)));
 
 var app = builder.Build();
